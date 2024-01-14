4ea88aebb5abdd58fcd1aca7e1702d92c42cdee1
Mateus Victor <mateusvictorct@gmail.com>
2024-01-14 11:33:33 -0300

    add arquivo da aula 157, e update do readme

---
+++ Curso_de_programação_C/Aula157.c
@@ -0,0 +1,29 @@
+// Cálculo do enésimo termo da sequência de Fibonacci
+// indices: 1 2 3 4 5 6 7 8  9  10...
+// némeros: 0 1 1 2 3 5 8 13 21 34 ...
+
+int Fibonacci(int x){
+
+    if ( x == 1){
+
+        return 0;
+    }else{
+        if(x == 2){
+            return 1;
+        } else{
+            return Fibonacci(x-1) + Fibonacci(x-2);
+        }
+    }
+}
+#include <stdio.h>
+int main(){
+    int n;
+
+    printf("Digite um valor:\n");
+    scanf("%d",&n);
+    
+    printf("O %d termo da sequencia e = %d\n",n,Fibonacci(n));
+
+
+    return 0;
+}
